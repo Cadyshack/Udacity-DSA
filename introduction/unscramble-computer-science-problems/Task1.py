d58a7e91f69591687aaf62168824d612af17616b
Christian Cadieux <cadieux_christian@hotmail.com>
2025-04-23 16:04:17 -0400

    Taask 1 complete

--- introduction/unscramble-computer-science-problems/Task1.py
+++ introduction/unscramble-computer-science-problems/Task1.py
@@ -18,3 +18,20 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+# Create an empty list to use to combine all numbers from texts and calls
+num_record = []
+# Loop through text records and add the telephone numbers to num_record, which are the first and second items of each record
+for record in texts:
+    num_record.append(record[0])
+    num_record.append(record[1])
+
+# Loop through the calls list and add the telephone numbers to num_record, which are the first, second and thrird items of each call record
+for record in calls:
+    num_record.append(record[0])
+    num_record.append(record[1])
+    num_record.append(record[2])
+
+# get the unique numbers
+unique_numbers = set(num_record)
+
+print(f"There are {len(unique_numbers)} different telephone numbers in the records.")
