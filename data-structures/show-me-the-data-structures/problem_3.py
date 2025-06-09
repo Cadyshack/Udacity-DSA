d04285db549456f37021c36f390028be213e7ac9
Christian Cadieux <cadieux_christian@hotmail.com>
2025-06-09 15:38:35 -0400

    Problem_3 working

--- data-structures/show-me-the-data-structures/problem_3.py
+++ data-structures/show-me-the-data-structures/problem_3.py
@@ -1,6 +1,7 @@
 import heapq
+import sys
 from collections import defaultdict
-from typing import Optional
+from typing import Optional, Any
 
 # Huffman Tree Node
 class HuffmanNode:
@@ -19,7 +20,7 @@ class HuffmanNode:
         The right child node.
     """
 
-    def __init__(self, char: Optional[str], freq: int) -> None:
+    def __init__(self, freq: int, char: Optional[str] = None, left: Optional['HuffmanNode'] = None, right: Optional['HuffmanNode'] = None) -> None:
         """
         Constructs all the necessary attributes for the HuffmanNode object.
 
@@ -30,7 +31,37 @@ class HuffmanNode:
         freq : int
             The frequency of the character.
         """
-        pass
+        self.freq = freq
+        self.char = char
+        self.left = None
+        self.right = None
+    
+    # set a node for the left child
+    def set_left_child(self,left) -> None:
+        self.left = left
+    
+    # set a node for the right child
+    def set_right_child(self, right) -> None:
+        self.right = right
+    
+    # get the node of left child
+    def get_left_child(self) -> Optional['HuffmanNode']:
+        return self.left
+    
+    # get the node of right child
+    def get_right_child(self) -> Optional['HuffmanNode']:
+        return self.right
+
+    # check if node has left child -> return boolean
+    def has_left_child(self) -> bool:
+        return self.left != None
+    
+    # check if node has right child -> return boolean
+    def has_right_child(self) -> bool:
+        return self.right != None
+    
+    def is_leaf(self) -> bool:
+        return self.left is None and self.right is None
 
     def __lt__(self, other: 'HuffmanNode') -> bool:
         """
@@ -46,7 +77,13 @@ class HuffmanNode:
         bool
             True if the frequency of this node is less than the other node, False otherwise.
         """
-        pass
+        return self.freq < other.freq
+    
+    def __str__(self) -> str:
+        return f"Node({(self.freq, self.char)})"
+    
+    def __repr__(self) -> str:
+        return f"Node({(self.freq, self.char)})"
 
 def calculate_frequencies(data: str) -> dict[str, int]:
     """
@@ -62,8 +99,12 @@ def calculate_frequencies(data: str) -> dict[str, int]:
     Dict[str, int]
         A dictionary with characters as keys and their frequencies as values.
     """
-    pass
-
+    d = defaultdict(int)
+    for char in data:
+        d[char] += 1
+    
+    return d
+    
 def build_huffman_tree(frequency: dict[str, int]) -> HuffmanNode:
     """
     Build the Huffman Tree based on the character frequencies.
@@ -78,9 +119,30 @@ def build_huffman_tree(frequency: dict[str, int]) -> HuffmanNode:
     HuffmanNode
         The root node of the constructed Huffman Tree.
     """
-    pass
+    # Create HuffmanNode for each dictionary item
+    pq = [HuffmanNode(freq, char) for char, freq in frequency.items()]
+    # Conver to a heap in O(k) time
+    heapq.heapify(pq)
 
-def generate_huffman_codes(node: Optional[HuffmanNode], code: str, huffman_codes: dict[str, str]) -> None:
+    while len(pq) > 1:
+        node_1 = heapq.heappop(pq)
+        node_2 = heapq.heappop(pq)
+        new_freq = node_1.freq + node_2.freq
+        new_node = HuffmanNode(new_freq)
+
+        if node_1.freq <= node_2.freq:
+            new_node.set_left_child(node_1)
+            new_node.set_right_child(node_2)
+        else:
+            new_node.set_left_child(node_2)
+            new_node.set_right_child(node_1)
+
+        heapq.heappush(pq, new_node)
+    
+    # Return the root node of the Huffman Tree
+    return heapq.heappop(pq)
+
+def generate_huffman_codes(node: Optional[HuffmanNode], code: str, huffman_codes: dict[str, str]) -> dict[str,str]:
     """
     Generate Huffman codes for each character by traversing the Huffman Tree.
 
@@ -93,9 +155,21 @@ def generate_huffman_codes(node: Optional[HuffmanNode], code: str, huffman_codes
     huffman_codes : Dict[str, str]
         A dictionary to store the generated Huffman codes.
     """
-    pass
-
+    
+    if node:
+        if node.is_leaf() and node.char is not None:
+            huffman_codes[node.char] = code
+        else:
+            code_left = code + "0"
+            generate_huffman_codes(node.left, code_left, huffman_codes)
+            code_right = code + "1"
+            generate_huffman_codes(node.right, code_right, huffman_codes)
+    
+    return huffman_codes
+            
 def huffman_encoding(data: str) -> tuple[str, Optional[HuffmanNode]]:
+
+    
     """
     Encode the given data using Huffman coding.
 
@@ -109,7 +183,16 @@ def huffman_encoding(data: str) -> tuple[str, Optional[HuffmanNode]]:
     Tuple[str, Optional[HuffmanNode]]
         A tuple containing the encoded string and the root of the Huffman Tree.
     """
-    pass
+    frequency_dict = calculate_frequencies(data)
+    tree = build_huffman_tree(frequency_dict)
+    huffman_codes = generate_huffman_codes(tree, "", {})
+
+    encoded_string = ""
+    for char in data:
+        encoded_string += huffman_codes[char]
+
+    return (encoded_string, tree)
+
 
 def huffman_decoding(encoded_data: str, tree: Optional[HuffmanNode]) -> str:
     """
@@ -127,12 +210,26 @@ def huffman_decoding(encoded_data: str, tree: Optional[HuffmanNode]) -> str:
     str
         The decoded string.
     """
-    pass
+    
+    decoded: str = ""
+    node = tree
 
+    for code in encoded_data:
+        if code == "0":
+            node = node.get_left_child()
+        elif code == "1":
+            node = node.get_right_child()
+        
+        if node.is_leaf():
+            decoded +=  node.char
+            node = tree
+
+    return decoded
 
 # Main Function
 if __name__ == "__main__":
     # Test Case 1: Standard test case
+
     print("\nTest Case 1: Standard sentence")
     sentence = "Huffman coding is fun!"
     encoded_data, tree = huffman_encoding(sentence)
@@ -142,8 +239,22 @@ if __name__ == "__main__":
     assert sentence == decoded_data
 
     # Test Case 2
-    pass
+    print("\nTest Case 2: Standard sentence 2")
+    a_great_sentence = "The bird is the word"
+
+    print(f"The size of the data is: {sys.getsizeof(a_great_sentence)}")
+    print(f"The content of the data is: {a_great_sentence}")
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print(f"The size of the encoded data is: {sys.getsizeof(int(encoded_data, base=2))}")
+    print(f"The content of the encoded data is: {encoded_data}")
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print(f"The size of the decoded data is: {sys.getsizeof(decoded_data)}")
+    print(f"The content of the encoded data is: {decoded_data}")
 
     # Test Case 3
-    pass
-
+    print("\nTest Case 3: Testing empty string input")
+    
